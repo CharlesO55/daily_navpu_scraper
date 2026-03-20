ba477c0ef02302067cbbccd0e781dbf98b2d4b1d
CharlesO55 <dymat98@gmail.com>
2026-03-21 01:09:51 +0800

    all banks

---
+++ data/uitf_web_keys.py
@@ -0,0 +1,18 @@
+# UITF.com.ph
+bank_ids = {
+    'metrobank': 1,
+    'pnb': 2,
+    'bpi': 3,
+    'aub': 4,
+    'boc': 5,
+    'bdo': 6,
+    'chinabank': 7,
+    'eastwest': 8,
+    'landbank': 9,
+    'pbcom': 10,
+    'rcbc': 11,
+    'securitybank': 12,
+    'unionbank': 13,
+    'ucpb': 14,
+}
+
--- scrapers/uitf_website.py
+++ scrapers/uitf_website.py
@@ -1,41 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
 import csv
 
-import re
 from os import makedirs
+from time import sleep
+import re
+from datetime import datetime
 
-OUTPUT_FOLDER = "data/chinabank"
-URL = 'https://uitf.com.ph/daily_navpu.php?bank_id=7'
-
-makedirs(OUTPUT_FOLDER, exist_ok=True)
-
-response = requests.get(URL)
-
-assert response.status_code == 200
-
-soup = BeautifulSoup(response.content, 'html.parser')
+from data.uitf_web_keys import bank_ids
 
 
-# DATE
-dt_str = soup.find('h2').get_text(strip=True)
-match = re.search(r'([A-Za-z]{3} \d{1,2}, \d{4})', dt_str)
-dt_str = match.group(1)
+def extract(bank_name : str, bank_id : int):
+    OUTPUT_FOLDER = f"data/{bank_name}"
+    URL = f'https://uitf.com.ph/daily_navpu.php?bank_id={bank_id}'
 
-dt = datetime.strptime(dt_str, '%b %d, %Y').date()
+    makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+    # SCRAPE
+    response = requests.get(URL)
+    assert response.status_code == 200
+    soup = BeautifulSoup(response.content, 'html.parser')
 
 
-# FUNDS
-start = soup.find(class_="table-title mt-5")
+    # DATE
+    dt_str = soup.find('h2').get_text(strip=True)
+    match = re.search(r'([A-Za-z]{3} \d{1,2}, \d{4})', dt_str)
+    dt_str = match.group(1)
 
-for tb in start.find_all_next('tbody'):
-    for tr in tb.find_all('tr'):
-        fund_name, fund_value = [td.get_text(strip=True) for td in tr.find_all('td')[:2]]
-        fund_name = fund_name.title().replace(" ", "_")
+    dt = datetime.strptime(dt_str, '%b %d, %Y').date()
 
-        with open(f"{OUTPUT_FOLDER}/{fund_name}.csv", 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([dt, fund_value])
-        
-        # print(f'{fund_name}: {fund_value}')
+
+    # FUNDS
+    start = soup.find(class_="table-title mt-5")
+
+    for tb in start.find_all_next('tbody'):
+        for tr in tb.find_all('tr'):
+            fund_name, fund_value = [td.get_text(strip=True) for td in tr.find_all('td')[:2]]
+            fund_name = fund_name.title().replace(" ", "_")
+
+            with open(f"{OUTPUT_FOLDER}/{fund_name}.csv", 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([dt, fund_value])
+            
+            # print(f'{fund_name}: {fund_value}')
+
+    sleep(0.5)
+
+if __name__ == "__main__":
+    for bank_name, bank_id in bank_ids.items():
+        extract(bank_name, bank_id)
